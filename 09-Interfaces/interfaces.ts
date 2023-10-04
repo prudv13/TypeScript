32516fae3ed9c1559294a7cdeeff878bdb37d078
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-04 22:18:44 +0530

    readonly, optional properties with interface

--- 09-Interfaces/interfaces.ts
+++ 09-Interfaces/interfaces.ts
@@ -4,3 +4,18 @@ interface Point {
 }
 
 const pt: Point = {x: 123, y: 1234};
+
+
+// readonly, optional properties
+interface Person {
+    readonly id: number;
+    first: string;
+    last: string;
+    nickName?: string;
+}
+
+const thomas: Person = {
+    id: 2134,
+    first: "Thomas",
+    last: "Hardy"
+};
