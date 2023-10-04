7a26e729d8380519129a73b232c66635aff7be00
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-04 22:27:02 +0530

    interface method parameters

--- 09-Interfaces/interfaces.ts
+++ 09-Interfaces/interfaces.ts
@@ -23,3 +23,24 @@ const thomas: Person = {
     sayHi: () => "Hi",
     sayHello: () => "Hello"
 };
+
+
+// --------------------------------------------
+
+interface Product {
+    name: string;
+    price: number;
+    applyDiscount(discount: number): number;
+}
+
+const shoes: Product = {
+    name: "Blue Suede Shoes",
+    price: 100,
+    applyDiscount(amount: number){
+        const newPrice = this.price * (1-amount);
+        this.price = newPrice;
+        return this.price;
+    }
+}
+
+shoes.applyDiscount(0.4);
