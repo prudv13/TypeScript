9757ebc764a6d4132832f4ec2f6b778eb19398c4
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-05 10:48:59 +0530

    class fields

--- 12-JS-Classes/classes.js
+++ 12-JS-Classes/classes.js
@@ -1,4 +1,8 @@
 class Player {
+    // class fields
+    score = 0;
+    numLives = 10;
+
     constructor(first, last){
         this.first = first;
         this.last = last;
@@ -7,14 +11,14 @@ class Player {
     taunt(){
         console.log("BOOYA!")
     }
+    loseLife(){
+        this.numLives -= 1;
+    }
 }
 
 const player1 = new Player("blue", "steele");
-player1.taunt();
-console.log(player1.first);
-console.log(player1.last);
+// player1.taunt();
+// console.log(player1.first);
+// console.log(player1.last);
 
-const player2 = new Player("charlie", "brown");
-player2.taunt();
-console.log(player2.first);
-console.log(player2.last);
+console.log(player1);
