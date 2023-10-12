eceb17022040bf4a4211ca9d85f0cb183439ff15
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-12 15:43:19 +0530

    public access modifier

--- 13-Typescript-Classes/src/index.ts
+++ 13-Typescript-Classes/src/index.ts
@@ -1,7 +1,7 @@
 class Player {
     readonly first: string;
     readonly last: string;
-    score: number = 0;
+    public score: number = 0;
 
     constructor(first: string, last: string){
         this.first = first;
