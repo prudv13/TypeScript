3c097ec063d7020983a373ff63fca30913ce1727
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-14 14:18:58 +0530

    builtin generics with DOM

--- 14-Generics/generics.ts
+++ 14-Generics/generics.ts
@@ -2,3 +2,12 @@
 
 const nums: Array<number> = [1,2,3,4];
 const colors: Array<string> = ["hello", "world"];
+
+const inputEl = document.querySelector<HTMLInputElement>("#username")!;
+console.dir(inputEl);
+inputEl.value = "Hacked!";
+
+const btn = document.querySelector<HTMLButtonElement>(".btn")!;
+
+// =====================================================================
+
