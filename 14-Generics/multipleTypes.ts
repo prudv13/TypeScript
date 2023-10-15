e33cb41927440c15c9e7c98458d5b03aa6225e77
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-10-15 14:37:07 +0530

    adding type constraints with generics

--- 14-Generics/multipleTypes.ts
+++ 14-Generics/multipleTypes.ts
@@ -1,4 +1,4 @@
-function merge<T,U>(object1: T, object2: U){
+function merge<T extends object,U extends object>(object1: T, object2: U){
     return {
         ...object1,
         ...object2
@@ -6,3 +6,15 @@ function merge<T,U>(object1: T, object2: U){
 }
 
 const comboObject = merge({name: "colt"}, {pets: ["blue", "elton"]});
+
+// ===============================================================
+
+interface Lengthy {
+    length: number;
+}
+
+function printDoubleLength<T extends Lengthy>(thing: T): number{
+    return thing.length*2;
+}
+
+printDoubleLength("hello");
